d0c7c107d6dbf729461867d3d57bb284e9c03c41
Robert Fields <robertfields411@gmail.com>
2025-10-25 12:38:07 -0400

    Game Over Logo Zoomed Out

--- LvglWindowsSimulator/ui/images/ui_image_logo.c
+++ LvglWindowsSimulator/ui/images/ui_image_logo.c
@@ -674,9 +674,8 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_IMG_LOGO 
 };
 
 const lv_img_dsc_t img_logo = {
-  .header.cf = LV_IMG_CF_RGB565A8,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.cf = LV_COLOR_FORMAT_RGB565,
+  .header.reserved_2 = 0,
   .header.w = 240,
   .header.h = 320,
   .data_size = 230400,
